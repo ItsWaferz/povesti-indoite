39b3e125862b91a7a20b8e54d6ca370da602054d
corneliu756 <162913180+ItsWaferz@users.noreply.github.com>
2025-08-28 08:30:46 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/povesti-indoite/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
